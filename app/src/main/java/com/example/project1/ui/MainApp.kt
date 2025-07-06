56f257ad664ca72270be5734b4dd1c2dc2d75eab
whshin <urihiya1@kaist.ac.kr>
2025-07-07 01:10:17 +0900

    7/7 0109

--- app/src/main/java/com/example/project1/ui/MainApp.kt
+++ app/src/main/java/com/example/project1/ui/MainApp.kt
@@ -1,13 +1,27 @@
-package com.example.project1.ui  // ← 너희 프로젝트 패키지에 맞게 수정
+package com.example.project1.ui
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.project1.R
+import com.example.project1.data.UserManager
+import com.example.project1.model.CafeInfo
+import com.example.project1.network.RetrofitClient
+import androidx.compose.runtime.getValue
 
 @Composable
 fun MainApp() {
     val navController = rememberNavController()
+
+    LaunchedEffect(Unit) {
+        val users = RetrofitClient.apiService.getUsers()
+        UserManager.currentUser = users.firstOrNull() // 일단은 first dummy user 자동 로그인
+    }
 
     NavHost(navController = navController, startDestination = "home") {
         composable("home") { HomeScreen(navController) }
@@ -16,7 +30,17 @@ fun MainApp() {
         composable("mylist") { MyCafeListScreen(navController) }
         composable("setting") { Setting(navController) }
         composable("info") { Profile(navController) }
-        composable("cafeinfo") { CafeInfo(navController) }
+        composable("cafeinfo/{cid}") { backStackEntry ->
+            val cid = backStackEntry.arguments?.getInt("cid")!!
+            var cafe by remember { mutableStateOf<CafeInfo?>(null) }
+
+            LaunchedEffect(cid) {
+                cafe = RetrofitClient.apiService.getCafeById(cid)
+            }
+            cafe?.let { cafe ->
+                CafeInfo(cafe, navController)
+            }
+        }
         composable("cafeDetail") {
             CafeDetailScreen(navController)
         }
